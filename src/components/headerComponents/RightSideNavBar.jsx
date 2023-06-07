b43e1bde5792d3e92db3f490ad5d8c65106db198
Kevin Fan <kevinlifan@yahoo.com>
2023-06-07 10:30:54 -0400

    updated resume

--- src/components/headerComponents/RightSideNavBar.jsx
+++ src/components/headerComponents/RightSideNavBar.jsx
@@ -6,7 +6,7 @@ import styles from './rightSide.module.scss';
 export default function RightSideNavBar() {
   return (
     <Nav className={styles.rightSide} >
-      <Nav.Link className={styles.rightSideLink} href='Kevin Fan Resume 3-2023.pdf'>
+      <Nav.Link className={styles.rightSideLink} href='Kevin Fan Resume.pdf'>
         Resume
       </Nav.Link>
       <Nav.Link className={styles.rightSideLink} href='https://github.com/kfan1'>
