1a4d15af37db7a030c33923a79820ea840d82cf5
Kevin Fan <kevinlifan@yahoo.com>
2023-06-21 16:52:11 -0400

    added link to rekuberate

--- src/components/headerComponents/RightSideNavBar.jsx
+++ src/components/headerComponents/RightSideNavBar.jsx
@@ -12,8 +12,8 @@ export default function RightSideNavBar() {
       <Nav.Link className={styles.rightSideLink} href='https://github.com/kfan1'>
         Projects
       </Nav.Link>
-      <Nav.Link className={styles.rightSideLink} href=''>
-        Blog
+      <Nav.Link className={styles.rightSideLink} href='https://rekuberate.org/'>
+        ReKuberate
       </Nav.Link>
       <DarkMode className={styles.rightSideLink} />
     </Nav>
