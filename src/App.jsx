e4ff490d4d995b13762d7d71650d7d5f865bc8f1
Kevin Fan <kevinlifan@yahoo.com>
2023-06-29 13:29:38 -0400

    loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderContainer from './containers/HeaderContainer.jsx';
 import FooterContainer from './containers/FooterContainer.jsx';
-
 import MainContainer from './containers/MainContainer.jsx';
+import styles from './stylesheets/AppStyles.module.scss';
 
 function App() {
   return (
     <div>
+      <div className={styles.loadingScreenLeft}></div>
+      <div className={styles.loadingScreenRight}></div>
       <HeaderContainer />
       <MainContainer />
       <FooterContainer />
