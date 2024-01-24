85ac235281f671cffff19d87330078fa8f4e8159
Kevin Fan <kevinlifan@yahoo.com>
2024-01-24 12:29:26 -0500

    updated loading screen and link onclick affect

--- src/components/headerComponents/RightSideNavBar.jsx
+++ src/components/headerComponents/RightSideNavBar.jsx
@@ -6,16 +6,16 @@ import styles from './rightSide.module.scss';
 export default function RightSideNavBar({ setProjects }) {
   return (
     <Nav className={styles.rightSide}>
-      <Nav.Link className={styles.rightSideLink} href='Kevin Fan Resume.pdf'>
+      <Nav.Link className={styles.rightSideLink} href='Kevin Fan Resume.pdf' target='_blank'>
         Resume
       </Nav.Link>
       <Nav.Link className={styles.rightSideLink} onClick={() => setProjects(true)}>
         Projects
       </Nav.Link>
-      <Nav.Link className={styles.rightSideLink} href='https://youtu.be/MOn4Sh1fZTM'>
+      <Nav.Link className={styles.rightSideLink} href='https://youtu.be/MOn4Sh1fZTM' target='_blank'>
         Public Talks
       </Nav.Link>
-      <Nav.Link className={styles.rightSideLink} href='https://rekuberate.org/'>
+      <Nav.Link className={styles.rightSideLink} href='https://rekuberate.org/' target='_blank'>
         ReKuberate
       </Nav.Link>
       <DarkMode className={styles.rightSideLink} />
